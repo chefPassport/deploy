aa682f6863bdb2f7d54bc2158d14a0cff5ee15df
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-02-07 11:56:14 -0500

    friday final edition 4real this time 4meow

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         <Route path="/login" component={Login} />
         <Route path="/home/recipe/:id" component={RecipePage} />
         <Route exact path="/home" component={HomePage} />
-        <Route path="/profile" component={Profile} />
+        <PrivateRoute path="/profile" component={Profile} />
       </Switch>
     </div>
   );
